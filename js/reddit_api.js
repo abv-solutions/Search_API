a079677e97f829d44c2062c1fda599f602a577c0
andreisuciu2005 <andreisuciu2005@yahoo.com>
2019-09-15 10:03:55 +0300

    fiiiiix

--- js/reddit_api.js
+++ js/reddit_api.js
@@ -1,6 +1,6 @@
 export default {
   search: function(searchTerm, sortBy, searchLimit) {
-    return fetch(`http://www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit}`)
+    return fetch(`https://www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit}`)
       .then(res => res.json())
       .then(data => data.data.children.map(data => data.data))
       .catch(err => console.log(err));
