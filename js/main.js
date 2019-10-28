66767630e22b3a37050620d94831d909b47c4514
Andrei Suciu <andreisuciu2005@yahoo.com>
2019-10-28 16:57:13 +0200

    prettier

--- js/main.js
+++ js/main.js
@@ -4,23 +4,23 @@ const searchForm = document.querySelector('#search-form');
 const searchInput = document.querySelector('#search-input');
 const search = document.querySelector('#search');
 
-searchForm.addEventListener('submit', e=> {
-  e.preventDefault();
-  const searchTerm = searchInput.value;
-  const sortBy = document.querySelector('input[name="sortby"]:checked').value;
-  const searchLimit = document.querySelector('#limit').value;
-  if (searchTerm === '') {
-    showAlert('Please type in a search term', 'danger');
-  }
-  searchInput.value = '';
-  reddit.search(searchTerm, sortBy, searchLimit)
-    .then(results => {
-      let output = '<div class="card-columns">';
-      results.forEach(post => {
-        let image = post.preview
-        ? post.preview.images[0].source.url
-        : 'https://cdn.comparitech.com/wp-content/uploads/2017/08/reddit-1.jpg';
-        output += `
+searchForm.addEventListener('submit', e => {
+	e.preventDefault();
+	const searchTerm = searchInput.value;
+	const sortBy = document.querySelector('input[name="sortby"]:checked').value;
+	const searchLimit = document.querySelector('#limit').value;
+	if (searchTerm === '') {
+		showAlert('Please type in a search term', 'danger');
+	}
+	searchInput.value = '';
+	reddit.search(searchTerm, sortBy, searchLimit)
+		.then(results => {
+			let output = '<div class="card-columns">';
+			results.forEach(post => {
+				let image = post.preview
+					? post.preview.images[0].source.url
+					: 'https://cdn.comparitech.com/wp-content/uploads/2017/08/reddit-1.jpg';
+				output += `
         <div class="card mb-2">
           <img class="card-img-top" src="${image}" alt="Card image cap">
           <div class="card-body">
@@ -33,24 +33,24 @@ searchForm.addEventListener('submit', e=> {
           </div>
         </div>
         `;
-      });
-      output += '</div>';
-      document.querySelector('#results').innerHTML = output;
-    });
+			});
+			output += '</div>';
+			document.querySelector('#results').innerHTML = output;
+		});
 });
 
 function showAlert(message, className) {
-  const div = document.createElement('div');
-  div.className = `alert alert-${className}`;
-  div.appendChild(document.createTextNode(message));
-  search.before(div);
-  setTimeout(() => document.querySelector('.alert').remove(), 2000);
+	const div = document.createElement('div');
+	div.className = `alert alert-${className}`;
+	div.appendChild(document.createTextNode(message));
+	search.before(div);
+	setTimeout(() => document.querySelector('.alert').remove(), 2000);
 }
 
 function truncateText(text, limit) {
-  const endIndex = text.indexOf(' ', limit);
-  if (endIndex == -1) {
-    return text;
-  }
-  return text.substring(0, endIndex);
+	const endIndex = text.indexOf(' ', limit);
+	if (endIndex == -1) {
+		return text;
+	}
+	return text.substring(0, endIndex);
 }
--- js/reddit_api.js
+++ js/reddit_api.js
@@ -1,8 +1,10 @@
 export default {
   search: function(searchTerm, sortBy, searchLimit) {
-    return fetch(`https://www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit}`)
+    return fetch(
+      `https://www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit}`
+    )
       .then(res => res.json())
       .then(data => data.data.children.map(data => data.data))
       .catch(err => console.log(err));
   }
-}
+};
